c38d39624b1c7662c59e7523ece87f6314d2e480
RitzTheCoder <ritzthecoder@gmail.com>
2025-03-15 17:46:02 +0530

    Add files via upload

---
+++ profit.py
@@ -0,0 +1,7 @@
+CP = float(input("Enter the Cost Price:"))
+SP = float(input("Enter the Sales Price:"))
+if (SP > CP):
+    a = SP - CP
+    print("The Total profit = {0}".format(a))
+else:
+    print("NO PROFIT!!")    
